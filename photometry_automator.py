b044af4bb0d5a0691229f79b1b3843eba6207149
ykawai6581 <yugo6581@g.ecc.u-tokyo.ac.jp>
2025-02-03 23:40:33 +0900

    edit

--- photometry_automator.py
+++ photometry_automator.py
@@ -428,7 +428,7 @@ class MuSCAT_PHOTOMETRY:
         for i in range(self.nccd):
             saturation_cids_per_ccd = []
             for star_id in range(int(self.nstars)):
-                count_above_threshold = (df[df["ccd"] == i][df["ID"] == star_id] > 60000).sum()
+                count_above_threshold = (df[i][df[i]["ID"] == star_id] > 60000).sum()
                 percentage_above_threshold = count_above_threshold / len(df[df["ccd"] == i][df["ID"] == star_id]) * 100
                 
                 # If more than 5% of the rows have a peak > 60000, add this star ID to the list
