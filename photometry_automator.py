107d837f0ad24b9bb4a8dc5d81e0e8809aee8ebc
ykawai6581 <yugo6581@g.ecc.u-tokyo.ac.jp>
2025-02-03 22:15:45 +0900

    edit

--- photometry_automator.py
+++ photometry_automator.py
@@ -242,11 +242,11 @@ class MuSCAT_PHOTOMETRY:
 
         # Assume the same available radius for all CCDs
         apphot_base = f"{self.obsdate}/{self.target}_0/apphot_{method}"
-        available_rad = [float(p.name[3:]) for p in Path(apphot_base).glob("*/")] if Path(apphot_base).exists() else []
-        available_rad.sort()
+        available_rad = sorted([float(p.name[3:]) for p in Path(apphot_base).glob("*/")]) if Path(apphot_base).exists() else []
 
+        print(available_rad)
         if available_rad and rad1==None and rad2==None and drad==None:
-            self.rad1, self.rad2, self.drad, self.method = float(available_rad[0]), float(available_rad[-1]), (available_rad[-1]-available_rad[0])/len(available_rad), method
+            self.rad1, self.rad2, self.method = float(available_rad[0]), float(available_rad[-1]), (available_rad[-1]-available_rad[0])/len(available_rad), method
             random_frame = self.obslog[0][self.obslog[0]["OBJECT"] == self.target]
             random_frame = int(random_frame["FRAME#1"].iloc[0])
             df, meta = self.read_photometry(ccd=0, rad=self.rad1, frame=random_frame, add_metadata=True)
